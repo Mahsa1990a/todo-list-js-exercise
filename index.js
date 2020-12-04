813747a65b7948bed5ece6919822123d81361832
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-12-04 16:30:21 +0000

    Use Methods Instead

--- index.js
+++ index.js
@@ -38,29 +38,60 @@
 // logTaskState(0); // Clean Cat Litter has been completed
 /////////////////////////////////////////////////////////////////////////////////////////
 
+// function newTask(title, description) {
+//   const task = {
+//     title: title,
+//     description: description,
+//     complete: false
+//   };
+//   return task;
+// }
+// // prints out the provided task's details
+// function logTaskState(task) {
+//   console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+// }
+// // marks the provided task as completed
+// function completeTask(task) {
+//   task.complete = true;
+// }
+
+// const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+// const task2 = newTask("Do Laundry", "😨");
+// const tasks = [task1, task2];
+
+// logTaskState(task1); // Clean Cat Litter has not been completed
+// completeTask(task1);
+// logTaskState(task1); // Clean Cat Litter has been completed
+
+// console.log(tasks);
+
+// //not obj oriented yet!
+//////////////////////////////////////////////////
+
+//OO:
+
 function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+
+    logState: function() { //logState: () => { ... } not working here
+      console.log(`${this.title} has ${this.complete ? " " : " not "}been completed.`)
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
   };
   return task;
-}
-// prints out the provided task's details
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-}
-// marks the provided task as completed
-function completeTask(task) {
-  task.complete = true;
 }
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-logTaskState(task1); // Clean Cat Litter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
 
-console.log(tasks);
